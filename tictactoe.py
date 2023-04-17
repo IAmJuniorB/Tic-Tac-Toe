a9929e6d2ae3d5e6875ace4a82dca6ee8d294b4f
Joe Bonfanti Jr <bonfanti7414@gmail.com>
2023-04-17 11:21:21 -0400

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,59 @@
+class TicTacToe:
+    BOARD_SIZE = 3
+
+    def __init__(self):
+        self.board = [[" " for _ in range(self.BOARD_SIZE)] for _ in range(self.BOARD_SIZE)]
+        self.players = ["X", "O"]
+        self.current_player = 0
+        self.scores = {player: 0 for player in self.players}
+        
+    def print_board(self):
+        for row in self.board:
+            print(row)
+
+    def make_move(self, row, col):
+        if self.board[row][col] == " ":
+            self.board[row][col] = self.players[self.current_player]
+            return True
+        return False
+
+    def get_move(self):
+        while True:
+            try:
+                row = int(input("Enter row (1-3): ")) - 1
+                col = int(input("Enter column (1-3): ")) - 1
+                if 0 <= row < self.BOARD_SIZE and 0 <= col < self.BOARD_SIZE:
+                    return row, col
+                else:
+                    print("Invalid input. Please enter row and column values between 1 and 3.")
+            except ValueError:
+                print("Invalid input. Please enter integer values for row and column.")
+
+    def check_win(self):
+        for row in self.board:
+            if row[0] == row[1] and row[1] == row[2] and row[0] != " ":
+                return row[0]
+
+        for col in range(self.BOARD_SIZE):
+            if self.board[0][col] == self.board[1][col] and self.board[1][col] == self.board[2][col] and \
+                    self.board[0][col] != " ":
+                return self.board[0][col]
+
+        if self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2] and self.board[0][0] != " ":
+            return self.board[0][0]
+
+        if self.board[2][0] == self.board[1][1] and self.board[1][1] == self.board[0][2] and self.board[2][0] != " ":
+            return self.board[0][0]
+
+        return None
+
+    def is_game_over(self):
+        for row in self.board:
+            for slot in row:
+                if slot == " ":
+                    return False
+        return True
+
     def reset_game_state(self):
         self.board = [[" " for _ in range(self.BOARD_SIZE)] for _ in range(self.BOARD_SIZE)]
         self.current_player = 0
@@ -44,3 +100,7 @@
                 else:
                     print("Thank you for playing Tic-Tac-Toe!")
                     break
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play()
